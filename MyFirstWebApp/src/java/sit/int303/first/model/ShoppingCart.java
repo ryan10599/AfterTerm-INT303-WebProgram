c6323b62642e4ff01d7d5e18ac2d6e32acbae17c
INT303 <INT303@TRAIN1_2-PC37.ad.sit.kmutt.ac.th>
2018-08-09 15:11:03 +0700

    [Upload]

---
+++ MyFirstWebApp/src/java/sit/int303/first/model/ShoppingCart.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int303.first.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import sit.int303.mockup.model.Product;
+
+/**
+ *
+ * @author INT303
+ */
+public class ShoppingCart implements Serializable{
+    private Map<String, LineItem> cart;
+    
+    public ShoppingCart(){
+        cart = new HashMap();
+    }
+    
+    public void add(Product p){
+        LineItem line = cart.get(p.getProductCode());
+        if (line == null){
+        cart.put(p.getProductCode(), new LineItem(p));
+        }else {
+            line.setQuantity(line.getQuantity()+1);
+        }
+    }
+    
+    public void remove(Product p){
+        this.remove(p.getProductCode());
+    }
+    
+    public void remove(String productCode){
+        cart.remove(productCode);
+    }
+    
+    public double getTotalPrice(){
+        double sum = 0;
+        Collection<LineItem> lineItems = cart.values();
+        for(LineItem lineItem : lineItems){
+            sum+=lineItem.getTotalPrice();
+        }
+        return sum;
+    }
+    public int getTotalQuatity(){
+        int sum = 0;
+        Collection<LineItem> lineItems = cart.values();
+        for(LineItem lineItem : lineItems){
+            sum+=lineItem.getQuantity();
+        }
+        return sum;
+    }
+    
+    public List<LineItem> getLineItems(){
+        return new ArrayList(cart.values());
+    }
+    
+}
