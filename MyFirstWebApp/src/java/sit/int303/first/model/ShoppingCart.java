1b0a736ced71104a086092ab6b01bbf40b384138
ryan10599 <35445881+ryan10599@users.noreply.github.com>
2018-08-10 15:37:09 +0700

    [Fix] Quantity

--- MyFirstWebApp/src/java/sit/int303/first/model/ShoppingCart.java
+++ MyFirstWebApp/src/java/sit/int303/first/model/ShoppingCart.java
@@ -49,7 +49,7 @@ public class ShoppingCart implements Serializable{
         }
         return sum;
     }
-    public int getTotalQuatity(){
+    public int getTotalQuantity(){
         int sum = 0;
         Collection<LineItem> lineItems = cart.values();
         for(LineItem lineItem : lineItems){
